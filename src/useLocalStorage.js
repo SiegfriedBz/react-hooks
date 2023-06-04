92fc8d827d2a515f619278514de8cefd2536ceb1
siegfriedbz <siegfried.bozza@yahoo.com>
2023-06-04 08:37:14 +0200

    refactor useLocalStorage

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -8,7 +8,7 @@ const getSavedValue = (key, initialValue) => {
   return initialValue;
 };
 
-export const useLocalStorage = (key, initialValue) => {
+export default function useLocalStorage(key, initialValue) => {
   const [value, setValue] = useState(() => {
     return getSavedValue(key, initialValue);
   });
